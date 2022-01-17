fa5a0a9596453f0d79729258184afddb756e197c
Anderlan Mota <contato@anderlan.com.br>
2022-01-17 13:53:08 -0300

    files

--- autoload.php
+++ autoload.php
@@ -1,7 +1,4 @@
 <?php 
-//error_reporting(E_ALL);
-//ini_set("log_errors", 1); 
-//ini_set('display_errors', 1);
 date_default_timezone_set('UTC');
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Methods: *');
@@ -11,21 +8,13 @@ header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 
+// permite a verificação se está acessível sem processar todo o sistema
 if (array_key_exists('REQUEST_METHOD', $_SERVER)) {
     if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {    
         return 0;    
     }    
 }
-
-
-
-exit("Dev");
-
-
-
-
-
-
+ 
 // antes de incluir um arquivo de uma classe instanciada, verifica se ele existe
 spl_autoload_register(function ($classname) {  
   $classincluded = false;
@@ -42,6 +31,21 @@ spl_autoload_register(function ($classname) {
     }
   }
 });
+
+// inclui o arquivo da versão solicitada
+if (array_key_exists('PATH_INFO', $_SERVER)) {
+	$elements = explode('/', $_SERVER['PATH_INFO']);
+} else {
+	
+	
+}
+        if (!array_key_exists('1', $this->elements)) {
+            $this->errormessage = "Acesso negado. Link inválido.";
+        } else { 
+			
+		}
+
+
 
 // inclui o arquivo principal dentro da pasta resource
 require_once dirname(__FILE__) . "/resource/main.php";
