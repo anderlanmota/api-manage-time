e933d78755938adde107b6ec6ed5520c20b44055
Anderlan Mota <contato@anderlan.com.br>
2022-01-17 16:59:31 -0300

    updates

--- resource/v1.php
+++ resource/v1.php
@@ -7,34 +7,34 @@ $mainResource = @array_key_last( RESOURCES );
 // verifica o recurso e chama a classe correta
 switch ( $mainResource ) {
   case 'auth':
-		$obj = new Auth();
-		$arr = $obj->run();
-		header( 'Content-Type: application/json' );
-		echo json_encode( $arr );
+    $obj = new Auth();
+    $arr = $obj->run();
+    header( 'Content-Type: application/json' );
+    echo json_encode( $arr );
     break;
   case 'users':
-		$obj = new User();
-		$arr = $obj->run();
-		header( 'Content-Type: application/json' );
-		echo json_encode( $arr );
+    $obj = new User();
+    $arr = $obj->run();
+    header( 'Content-Type: application/json' );
+    echo json_encode( $arr );
     break;
   case 'projects':
-		$obj = new Project();
-		$arr = $obj->run();
-		header( 'Content-Type: application/json' );
-		echo json_encode( $arr );
+    $obj = new Project();
+    $arr = $obj->run();
+    header( 'Content-Type: application/json' );
+    echo json_encode( $arr );
     break;
   case 'times':
-		$obj = new ManageTime();
-		$arr = $obj->run();
-		header( 'Content-Type: application/json' );
-		echo json_encode( $arr );
+    $obj = new ManageTime();
+    $arr = $obj->run();
+    header( 'Content-Type: application/json' );
+    echo json_encode( $arr );
     break;
-case 'maintenance':
-		$obj = new Auth();
-		$arr = $obj->run();
-		header( 'Content-Type: application/json' );
-		echo json_encode( $arr );
+  case 'maintenance':
+    $obj = new Maintenance();
+    $arr = $obj->run();
+    header( 'Content-Type: application/json' );
+    echo json_encode( $arr );
     break;
   default:
     http_response_code( 404 );
