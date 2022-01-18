569134d1f788cc2729f9bc672b42929e7621e44f
Anderlan Mota <contato@anderlan.com.br>
2022-01-18 14:18:49 -0300

    updates

--- model/ManageTime.php
+++ model/ManageTime.php
@@ -16,7 +16,7 @@ class ManageTime extends Auth {
   // Api Public: NO
   private function post(){
 	  $Sanitizer = new Sanitizer();
-	  $started = @$Sanitizer->datetime(DATA['started']);
+	  $started = DATA['started'];
 	  $ended = @$Sanitizer->datetime(DATA['started']);
 	  $userId = @RESOURCES['users'];
 	  $projectId = @RESOURCES['projects'];
