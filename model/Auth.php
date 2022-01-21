43718af5c84df01d23638ab3628267a9aabd4f73
Anderlan Mota <contato@anderlan.com.br>
2022-01-21 14:05:44 -0300

    news updates

--- model/Auth.php
+++ model/Auth.php
@@ -175,10 +175,10 @@ class Auth extends Database {
         $signature_provided = $tokenParts[ 2 ];
         $expiration = json_decode( $payload )->exp;
         $is_token_expired = ( $expiration - time() ) < 0;
-        $base64_url_header = base64url_encode( $header );
-        $base64_url_payload = base64url_encode( $payload );
+        $base64_url_header = $this->base64url_encode( $header );
+        $base64_url_payload = $this->base64url_encode( $payload );
         $signature = hash_hmac( 'SHA256', $base64_url_header . "." . $base64_url_payload, $secret, true );
-        $base64_url_signature = base64url_encode( $signature );
+        $base64_url_signature = $this->base64url_encode( $signature );
         $is_signature_valid = ( $base64_url_signature === $signature_provided );
         if ( $is_token_expired ) {
           return false;
